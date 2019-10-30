e06bcec91c7cae2e1a3744345e69fab0450a7247
KuzminKirill <wee3dood@gmail.com>
2019-10-30 14:41:38 +0300

    add error and success message

--- resources/views/blog/admin/categories/edit.blade.php
+++ resources/views/blog/admin/categories/edit.blade.php
@@ -5,6 +5,37 @@
     <form method="POST" action="{{ route('blog.admin.categories.update', $item->id ) }}">
         @method('PATCH')
         @csrf
+
+        @php
+        /** @var \Illuminate\Support\ViewErrorBag $errors */
+        @endphp
+
+        @if($errors->any())
+            <div class="row justify-content-center">
+                <div class="col-md-11">
+                    <div class="alert alert-danger" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">x</span>
+                        </button>
+                        {{ $errors->first() }}
+                    </div>
+                </div>
+            </div>
+        @endif
+
+        @if(session('success'))
+            <div class="row justify-content-center">
+                <div class="col-md-11">
+                    <div class="alert alert-success" role="alert">
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">x</span>
+                        </button>
+                        {{ session()->get('success') }}
+                    </div>
+                </div>
+            </div>
+        @endif
+        
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8">
